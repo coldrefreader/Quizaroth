52f400bb95b082eed335035bdc9fc755c9c277e0
Viktor Gochev <viktor.gochev1@gmail.com>
2025-04-11 15:47:34 +0300

    Question importer now checks for new questions and swapped out system printing with logs

--- src/main/java/app/question/repository/QuestionRepository.java
+++ src/main/java/app/question/repository/QuestionRepository.java
@@ -2,15 +2,13 @@ package app.question.repository;
 
 import app.question.model.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, UUID> {
 
-    @Query(value = "SELECT * FROM questions ORDER BY RAND() LIMIT 10", nativeQuery = true)
-    List<Question> getRandomQuestions();
+    Optional<Question> findByText(String text);
 }
--- src/main/java/app/question/service/CsvImporterService.java
+++ src/main/java/app/question/service/CsvImporterService.java
@@ -3,6 +3,7 @@ package app.question.service;
 import app.question.model.Question;
 import app.question.repository.QuestionRepository;
 import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
@@ -14,6 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@Slf4j
 public class CsvImporterService {
 
     private final QuestionRepository questionRepository;
@@ -24,33 +26,32 @@ public class CsvImporterService {
         this.questionRepository = questionRepository;
     }
 
-    @PostConstruct
-    public void importQuestions(){
 
-        if (questionRepository.count() > 0) {
-            System.out.println("Questions are already imported, skipping...");
-            return;
-        }
+
+    @PostConstruct
+    public void importQuestions()  {
+
+        List<Question> newQuestions = new ArrayList<>();
 
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(
                 new ClassPathResource("static/csv/questions.csv").getInputStream(), StandardCharsets.UTF_8))) {
 
             String line;
-            List<Question> questions = new ArrayList<>();
-
             boolean firstLine = true;
 
             while ((line = reader.readLine()) != null) {
-
                 if (firstLine) {
                     firstLine = false; // Header is info about what the questions do, so it's skipped
                     continue;
                 }
 
-                System.out.println(line);
-
                 String[] tokens = line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", - 1);
                 if (tokens.length < 2) {
+                    continue;
+                }
+
+                String text = tokens[1].replaceAll("\"", "");
+                if (questionRepository.findByText(text).isPresent()) {
                     continue;
                 }
 
@@ -62,14 +63,17 @@ public class CsvImporterService {
                         tokens[4].replace("\"", "")));
                 question.setCorrectAnswerIndex(Integer.parseInt(tokens[5]));
 
-                questions.add(question);
+                newQuestions.add(question);
             }
-
-            questionRepository.saveAll(questions);
-            System.out.println("Successfully imported " + questions.size() + " questions");
-
         } catch (Exception e) {
             e.printStackTrace();
+        }
+
+        if (!newQuestions.isEmpty()) {
+            questionRepository.saveAll(newQuestions);
+            log.info("Successfully imported {} question/s", newQuestions.size());
+        } else {
+            log.info("No new questions found");
         }
     }
 }
